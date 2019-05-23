7e0ff85993ac70ef63f4888255bf3289ba4eba13
gypsymoth94 <50933247+gypsymoth94@users.noreply.github.com>
2019-05-22 20:12:25 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,23 +5,68 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/Brian Strohm$/;
+      botRegex1 = /^\/Brian Strohm$/;
+	  botRegex2 = /^\/Bitchfuck$/;
 
-  if(request.text && botRegex.test(request.text)) {
+  if(request.text && botRegex1.test(request.text)) {
     this.res.writeHead(200);
-    postMessage();
+    postMessage1();
     this.res.end();
-  } else {
+  }
+  
+  else if(request.text && botRegex2.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage2();
+    this.res.end();
+  }
+  
+  else {
     console.log("don't care");
     this.res.writeHead(200);
     this.res.end();
   }
 }
 
-function postMessage() {
+function postMessage1() {
   var botResponse, options, body, botReq;
 
-  botResponse = "Hey, thats me!  My name is Brian Strohm & I am a BOT.  You know it, I know it;  Everybody knows it.";
+  botResponse = "Hey, thats me!  My name is Brian Strohm & I am a BOT.";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+
+function postMessage2() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "Fuck you you fucking faggot";
 
   options = {
     hostname: 'api.groupme.com',
