840acc2f653ef40c6a7cc269984cd0b58aeb67a5
gypsymoth94 <50933247+gypsymoth94@users.noreply.github.com>
2019-05-23 04:10:32 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -29,6 +29,7 @@ function respond() {
 	  botRegexL = /^\/omw$/;
 	  botRegexM = /^\/neat$/;
 	  botRegexN = /^\/WOAH$/;
+	  botRegexO = /^\/POGGERS$/;
 
 	  
 
@@ -173,6 +174,11 @@ function respond() {
   else if(request.text && botRegexN.test(request.text)) {
     this.res.writeHead(200);
     postMessageN();
+    this.res.end();
+  }
+  else if(request.text && botRegexO.test(request.text)) {
+    this.res.writeHead(200);
+    postMessageO();
     this.res.end();
   }
   
@@ -962,6 +968,41 @@ function postMessageN() {
   var botResponse, options, body, botReq;
 
   botResponse = "https://i.imgur.com/GezeA9G.png";
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+function postMessageO() {
+  var botResponse, options, body, botReq;
+
+  botResponse = "https://i.imgur.com/KBsZMNB.png";
 
   options = {
     hostname: 'api.groupme.com',
